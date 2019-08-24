220330d46b31508c10833b85a9a9190d741523e8
XorTroll <xabofdez@outlook.es>
2019-08-24 12:58:43 +0200

    Fix example, improve build/clean scripts

--- hello-world/build.sh
+++ hello-world/build.sh
@@ -1,2 +1,27 @@
-#!/bin/sh
-xargo build --release --target aarch64-horizon-nro
+#!/bin/bash
+set -e
+target=aarch64-horizon-elf
+type=nro
+
+CARGO_INCREMENTAL=0 RUST_TARGET_PATH=$PWD RUST_BACKTRACE=1 xargo build --release --target $target
+
+for elf in $PWD/target/$target/release/*.elf; do
+    outelf=$elf
+    break 1
+done
+
+if [ "$type" == "elf" ]; then
+    elfname=$(basename -- "$PWD/$outelf")
+    newelf=$PWD/$elfname
+    cp -r $elf $newelf
+elif [ "$type" == "nro" ]; then
+    elfname=$(basename -- "$PWD/$outelf")
+    outnro="$PWD/${elfname%.*}".nro
+    /opt/devkitpro/tools/bin/elf2nro $elf $outnro
+    echo "Generated NRO: $outnro"
+elif [ "$type" == "nso" ]; then
+    elfname=$(basename -- "$PWD/$outelf")
+    outnso="$PWD/${elfname%.*}".nso
+    /opt/devkitpro/tools/bin/elf2nso $elf $outnso
+    echo "Generated NSO: $outnso"
+fi
---
+++ hello-world/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+xargo clean
+find $PWD -name "*.elf" -type f -delete
+find $PWD -name "*.nro" -type f -delete
+find $PWD -name "*.nso" -type f -delete
